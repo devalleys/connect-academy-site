25b82fc086af7193ab01aae917c25e580e76accd
Niel Berongoy <niel@trouthousetech.com>
2025-06-25 02:07:49 +0800

    update navbar and package script

--- apps/connect-academy/src/components/Layout/Navbar/Navbar.tsx
+++ apps/connect-academy/src/components/Layout/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const Navbar: React.FC = () => {
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-transparent shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
