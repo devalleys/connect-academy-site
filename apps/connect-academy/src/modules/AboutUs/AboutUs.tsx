5024eda6f5523a8351ab4edf8698c740431033b8
Niel Berongoy <niel_berongoy@yahoo.com>
2025-06-27 02:36:28 +0800

    added partners (#14)
    * added partners
    
    * added partners
    
    * added partner logo
    
    * added partners
    
    ---------
    
    Co-authored-by: Niel Berongoy <niel@trouthousetech.com>

---
+++ apps/connect-academy/src/modules/AboutUs/AboutUs.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const AboutUs: React.FC = () => {
+  return (
+    <div className="h-[800px] bg-blue-950 flex flex-col items-center justify-center w-full">
+      <h1 className="text-white text-4xl font-bold text-center mt-20">
+        Insert about us here
+      </h1>
+    </div>
+  );
+};
